a6cb8fd9f57571db190eed146626cbbbfe084a91
Daniel Bettles <danbettles@yahoo.co.uk>
2022-12-02 20:39:37 +0000

    Tweaked the 404 error message.

--- templates/_errors/error_404.html.php
+++ templates/_errors/error_404.html.php
@@ -3,5 +3,5 @@
 // phpcs:ignore
 echo $output->include('_errors/error.html.php', [
     'metaTitle' => 'Not Found',
-    'detail' => 'I would have thought twice about following that path.  The ground disappears from beneath your feet and you are lost to the void.',
+    'detail' => 'I would have thought twice about following that path.  The ground disappears from beneath your feet and you are left staring into the abyss.',
 ]);
