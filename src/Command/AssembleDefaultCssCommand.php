a504bea2387253fc37df429adbef7d1f7ae1de8d
Daniel Bettles <danbettles@yahoo.co.uk>
2023-01-17 17:21:28 +0000

    Updated the build-CSS command: the CSS is no longer minified, because it shouldn't be.

--- src/Command/AssembleDefaultCssCommand.php
+++ src/Command/AssembleDefaultCssCommand.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Miniblog\Engine\Command;
 
-use DanBettles\Marigold\CssMinifier;
 use Exception;
+use Miniblog\Engine\Console;
 use RuntimeException;
 
+use function array_combine;
 use function array_map;
 use function file_get_contents;
 use function file_put_contents;
@@ -31,57 +32,66 @@ class AssembleDefaultCssCommand extends AbstractCommand
     /**
      * @var string[]
      */
-    private const SOURCE_STYLESHEETS = [
-        'vendor/csstools/sanitize.css/sanitize.css',
-        'vendor/csstools/sanitize.css/assets.css',
-        'assets/app.css',
-    ];
+    private array $sourceStylesheetPathnames;
 
-    /**
-     * @var string
-     */
-    private const OUTPUT_STYLESHEET = 'templates/stylesheet.css';
+    private string $outputFilePathname;
+
+    public function __construct(Console $console)
+    {
+        parent::__construct($console);
+
+        /** @var array<string,mixed> */
+        $config = $this->get('config');
+
+        /** @var string */
+        $engineDir = $config['engineDir'];
+        /** @var string */
+        $engineTemplatesDir = $config['engineTemplatesDir'];
+
+        // Config.
+        $cssFileRelPathnames = [
+            'vendor/csstools/sanitize.css/sanitize.css',
+            'vendor/csstools/sanitize.css/assets.css',
+            'assets/app.css',
+        ];
+
+        $this->sourceStylesheetPathnames = array_map(function (string $relPathname) use ($engineDir): string {
+            return "{$engineDir}/{$relPathname}";
+        }, array_combine($cssFileRelPathnames, $cssFileRelPathnames));
+
+        // Config.
+        $this->outputFilePathname = "{$engineTemplatesDir}/stylesheet.css";
+    }
 
     public function __invoke(): int
     {
-        /** @var array<string,mixed> */
-        $config = $this->get('config');
-        /** @var string */
-        $engineDir = $config['engineDir'];
+        $sourceStylesheets = [];
 
-        $cssMinifier = new CssMinifier();
-
-        $mergedCss = implode(PHP_EOL, array_map(function (string $cssFileRelPathname) use (
-            $engineDir,
-            $cssMinifier
-        ): string {
-            $cssFileAbsPathname = "{$engineDir}/{$cssFileRelPathname}";
-
-            if (!is_file($cssFileAbsPathname)) {
-                throw new RuntimeException("File `{$cssFileAbsPathname}` does not exist");
+        foreach ($this->sourceStylesheetPathnames as $relPathname => $absPathname) {
+            if (!is_file($absPathname)) {
+                throw new RuntimeException("File `{$absPathname}` does not exist");
             }
 
             /** @var string */
-            $sourceCss = file_get_contents($cssFileAbsPathname);
-            $minifiedCss = trim($cssMinifier->removeCommentsFilter($sourceCss));
+            $sourceCss = file_get_contents($absPathname);
 
-            return "/* ###> `{$cssFileRelPathname}` ### */" . PHP_EOL . $minifiedCss;
-        }, self::SOURCE_STYLESHEETS));
-
-        // Remove superfluous newlines to make the CSS a little more compact.
-        $mergedCss = preg_replace('~(\r\n|\r|\n){2,}~', PHP_EOL, $mergedCss) . PHP_EOL;
-
-        $outputFilePathname = "{$engineDir}/" . self::OUTPUT_STYLESHEET;
-
-        if (!(bool) file_put_contents($outputFilePathname, $mergedCss)) {
-            throw new RuntimeException("Failed to create `{$outputFilePathname}`");
+            $sourceStylesheets[] = "/* ###> {$relPathname} ### */" . PHP_EOL . PHP_EOL . trim($sourceCss);
         }
 
-        $this->getConsole()->writeLn("Created {$outputFilePathname}");
+        $mergedCss = implode(PHP_EOL . PHP_EOL, $sourceStylesheets);
+
+        // Remove superfluous newlines to make the CSS neater and a little more compact.
+        $mergedCss = preg_replace('~(\r\n|\r|\n){3,}~', PHP_EOL, $mergedCss) . PHP_EOL;
+
+        if (!(bool) file_put_contents($this->outputFilePathname, $mergedCss)) {
+            throw new RuntimeException("Failed to create `{$this->outputFilePathname}`");
+        }
+
+        $this->getConsole()->writeLn("Created `{$this->outputFilePathname}`");
 
         // It's not the end of the world if this fails.
         try {
-            $this->getConsole()->passthru("git add -v {$outputFilePathname}");
+            $this->getConsole()->passthru("git add -v {$this->outputFilePathname}");
         } catch (Exception $ignore) {
         }
 
